8da0488efc0a62701a7dcba651295bcf29c36fbf
louisdebray <louisdebray345@gmail.com>
2026-08-16 19:07:40 +0200

    Delete onside-github-1 directory

--- onside-github-1/frontend/src/domain/competitions.js
+++
@@ -1,8 +0,0 @@
-export const DEFAULT_COMPETITIONS = [
-  { code: 'PL', name: 'Premier League' },
-  { code: 'FL1', name: 'Ligue 1' },
-  { code: 'PD', name: 'Liga' },
-  { code: 'SA', name: 'Serie A' },
-  { code: 'BL1', name: 'Bundesliga' },
-  { code: 'CL', name: 'Ligue des Champions' },
-]
